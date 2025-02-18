40df3d5a1bec6f51867336058b3aa9f70d9fe290
neitoo <neito.dev@gmail.com>
2025-02-18 17:38:09 +0500

    ✨ Add the Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import Layout from '@components/Layout'
+import Home from '@pages/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
-    <>
-      <h1 className="hello-text">Hello world!</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
